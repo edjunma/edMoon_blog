4b32bcc6ebfe0583cc2f5a543ff63a79cf190af6
edjunma <edjunma@gmail.com>
2021-11-17 01:22:25 -0500

    setup graphcms api permissions, start on postcard component

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import { PostCard, Categories, PostWidget } from '../components';
 import { getPosts } from '../services';
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
